e6950e1ecd98ff12526239aa932a0120269f6294
Harshavardhan D B <harshavardhan@asirtech.local>
2024-05-11 20:04:48 +0530

    adding a new method

---
+++ src/test/java/test/AddTwoNumbers.java
@@ -0,0 +1,15 @@
+package test;
+
+public class AddTwoNumbers {
+
+	void add(int a , int b) {
+		int result = a+b;
+		System.out.println(result);
+	}
+	
+	public static void main(String[] args) {
+
+		
+	}
+
+}
